17b588e7caeb2dba1fdff833935179f33ae7a8bd
faridrawing <muhammad.farid.yamin@gmail.com>
2024-08-22 10:45:08 +0700

    feat: add paragraph

--- index.php
+++ index.php
@@ -7,5 +7,8 @@
 </head>
 <body>
   <h1>Hello world!</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid non reiciendis sint molestiae quasi nulla repellat temporibus, similique veniam, fugiat suscipit, libero dicta. Dolor, temporibus vel nesciunt unde quae ipsum vitae est libero praesentium tempora quod voluptatem enim, harum veritatis. Inventore nulla repudiandae molestiae atque eum dolores recusandae aspernatur similique odit officia, nesciunt nostrum, dolorem laboriosam quod reiciendis itaque sapiente fugit numquam, quae aperiam quam dolor cupiditate error! Doloribus reprehenderit ratione tempore repellat quasi nulla dolor, libero fuga excepturi veniam harum eos. Voluptas eius quod quos ad ipsam, deleniti repellendus sed dolorem sit? Labore animi nisi quas, reprehenderit esse quia?
+  </p>
 </body>
 </html>
